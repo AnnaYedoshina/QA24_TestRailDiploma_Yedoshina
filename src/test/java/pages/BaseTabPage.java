ee45ef142f989bf29c8a35940aca42bcc53a0c71
AnnaYedoshina <yedoshina.java@gmail.com>
2023-08-09 12:32:21 +0300

    some mistakes corrected

--- src/test/java/pages/BaseTabPage.java
+++ src/test/java/pages/BaseTabPage.java
@@ -4,8 +4,6 @@ import elements.Checkbox;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import java.util.List;
 
 public class BaseTabPage extends BasePage{
