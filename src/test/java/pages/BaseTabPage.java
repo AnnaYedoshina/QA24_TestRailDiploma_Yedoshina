fe762ccd75ffff42bf094934cc807215922a54cb
AnnaYedoshina <yedoshina.java@gmail.com>
2023-08-09 13:12:02 +0300

    some mistakes corrected

--- src/test/java/pages/BaseTabPage.java
+++ src/test/java/pages/BaseTabPage.java
@@ -4,14 +4,16 @@ import elements.Checkbox;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 import java.util.List;
 
-public class BaseTabPage extends BasePage{
+public class BaseTabPage extends BasePage {
     private static final By CONFIRM_DELETE_BUTTON = By.xpath("//*[@id='deleteDialog']/descendant::a[contains(@class,'button-ok')]");
 
     public BaseTabPage(WebDriver driver) {
         super(driver);
     }
+
     public boolean isEntityExist(String entityName, By entityLocator) {
         List<WebElement> entitiesList = driver.findElements(entityLocator);
         boolean isEntityExist = false;
@@ -22,6 +24,7 @@ public class BaseTabPage extends BasePage{
         }
         return isEntityExist;
     }
+
     public void confirmDelete() {
         new Checkbox(driver, CONFIRM_DELETE_BUTTON).check();
     }
