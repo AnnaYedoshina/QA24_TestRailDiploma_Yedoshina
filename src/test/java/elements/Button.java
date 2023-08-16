2b1fc80a49e468de848862deecb07301f17f10d6
AnnaYedoshina <yedoshina.java@gmail.com>
2023-08-16 18:35:45 +0300

    Modals added

--- src/test/java/elements/Button.java
+++ src/test/java/elements/Button.java
@@ -3,7 +3,6 @@ package elements;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 public class Button {
     private final MyElement element;
