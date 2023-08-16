36366c0c8ce432cf0340ae0887b47dfa7b39dc23
AnnaYedoshina <yedoshina.java@gmail.com>
2023-08-16 18:40:33 +0300

    Modals added

--- src/test/java/elements/Button.java
+++ src/test/java/elements/Button.java
@@ -6,20 +6,16 @@ import org.openqa.selenium.WebElement;
 
 public class Button {
     private final MyElement element;
-    private final WebDriver driver;
 
     public Button(WebDriver driver, By by) {
-        this.driver = driver;
         this.element = new MyElement(driver, by);
     }
 
     public Button(WebDriver driver, WebElement element) {
-        this.driver = driver;
         this.element = new MyElement(driver, element);
     }
 
     public Button(WebDriver driver, String id) {
-        this.driver = driver;
         this.element = new MyElement(driver, By.id(id));
     }
 
